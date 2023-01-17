d280b85a667ca83c5067416b419bc9f7ef032fcf
Леонид <leonis13579@gmail.com>
2023-01-17 20:13:34 +0300

    Work queues tutorial

--- src/main/java/com/example/rabbitmqtest/work_queues/WorkQueuesConfig.java
+++ src/main/java/com/example/rabbitmqtest/work_queues/WorkQueuesConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile({"tut2", "work-queues"})
+@Profile({"work_queues"})
 @Configuration
-public class Tut2Config {
+public class WorkQueuesConfig {
 
     @Bean
     public Queue hello() {
@@ -18,19 +18,19 @@ public class Tut2Config {
     private static class ReceiverConfig {
 
         @Bean
-        public Tut2Receiver receiver1() {
-            return new Tut2Receiver(1);
+        public WorkQueuesReceiver receiver1() {
+            return new WorkQueuesReceiver(1);
         }
 
         @Bean
-        public Tut2Receiver receiver2() {
-            return new Tut2Receiver(2);
+        public WorkQueuesReceiver receiver2() {
+            return new WorkQueuesReceiver(2);
         }
     }
 
     @Profile("sender")
     @Bean
-    public Tut2Sender sender() {
-        return new Tut2Sender();
+    public WorkQueuesSender sender() {
+        return new WorkQueuesSender();
     }
 }

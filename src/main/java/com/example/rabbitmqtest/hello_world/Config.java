63e64a7e980b472a243cd61821e18c6b684fe06b
Леонид <leonis13579@gmail.com>
2023-01-17 20:10:46 +0300

    Hello world tutorial

--- src/main/java/com/example/rabbitmqtest/hello_world/Config.java
+++ src/main/java/com/example/rabbitmqtest/hello_world/Config.java
@@ -1,2 +1,27 @@
-package com.example.rabbitmqtest.hello_world;public class config {
+package com.example.rabbitmqtest.hello_world;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"hello_world"})
+@Configuration
+public class Config {
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    @Bean
+    public HelloWorldReceiver receiver() {
+        return new HelloWorldReceiver();
+    }
+
+    @Profile("sender")
+    @Bean
+    public HelloWorldSender sender() {
+        return new HelloWorldSender();
+    }
 }

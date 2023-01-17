940d30d2804a4e3d8d30c5ed52d7d62edfe356e4
Леонид <leonis13579@gmail.com>
2023-01-17 20:14:02 +0300

    PubSub tutorial

---
+++ src/main/java/com/example/rabbitmqtest/pub_sub/PubSubConfig.java
@@ -0,0 +1,54 @@
+package com.example.rabbitmqtest.pub_sub;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
+@Profile({"pub_sub"})
+@Configuration
+public class PubSubConfig {
+
+    @Bean
+    public FanoutExchange fanout() {
+        return new FanoutExchange("tut.fanout");
+    }
+
+    @Profile("receiver")
+    private static class ReceiverConfig {
+
+        @Bean
+        public Queue autoDeleteQueue1() {
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Queue autoDeleteQueue2() {
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Binding binding1(FanoutExchange fanout,
+                                Queue autoDeleteQueue1) {
+            return BindingBuilder.bind(autoDeleteQueue1).to(fanout);
+        }
+
+        @Bean
+        public Binding binding2(FanoutExchange fanout,
+                                Queue autoDeleteQueue2) {
+            return BindingBuilder.bind(autoDeleteQueue2).to(fanout);
+        }
+
+        @Bean
+        public PubSubReceiver receiver() {
+            return new PubSubReceiver();
+        }
+    }
+
+    @Profile("sender")
+    @Bean
+    public PubSubSender sender() {
+        return new PubSubSender();
+    }
+}
--- src/main/java/com/example/rabbitmqtest/pub_sub/PubSubMain.java
+++ src/main/java/com/example/rabbitmqtest/pub_sub/PubSubMain.java
@@ -1,2 +1,24 @@
-package com.example.rabbitmqtest.pub_sub;public class PubSubMain {
+package com.example.rabbitmqtest.pub_sub;
+
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("pub_sub")
+public class PubSubMain implements CommandLineRunner {
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private FanoutExchange fanout;   // configured in Tut3Config above
+
+    @Override
+    public void run(String... args) throws Exception {
+        template.convertAndSend(fanout.getName(), "", "Message from pub_sub");
+    }
 }
